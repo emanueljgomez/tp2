9ea74fb4d7593345d1ccc3a0dcd7a5113f30ed91
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-07 17:50:46 -0300

    Added a sign up component and form validation

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  myForm:FormGroup
+
+  constructor(
+    private fb:FormBuilder
+  ) {
+    this.myForm=this.fb.group({
+    name:["",[Validators.required]],
+    lastname:["",[Validators.required]],
+    phone:[""],
+    user:["",[Validators.required]],
+    password:["",[Validators.required,Validators.minLength(6),Validators.maxLength(10)]],
+  })
+}
+
+registro(){
+  console.log(this.myForm.value)
+}
 
   ngOnInit(): void {
   }
